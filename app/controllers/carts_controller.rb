2096efd2b202d545e3820430b40af75986a77b8b
nikola-klobucar <nikola.klobucar1@gmail.com>
2022-01-14 11:50:27 +0100

    Front end styling

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,10 @@
 class CartsController < ApplicationController
 
     def show
-        @order_items = current_order.order_items
+        if user_signed_in?
+            @order_items = current_order.order_items
+        else
+            redirect_to new_user_session_path
+        end
     end
 end
