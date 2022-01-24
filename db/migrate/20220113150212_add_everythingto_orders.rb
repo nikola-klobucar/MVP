1d7605e9e6697a41f818a23846556d181aca7130
nikola-klobucar <nikola.klobucar1@gmail.com>
2022-01-24 11:29:08 +0100

    Removed limits in migrations

--- db/migrate/20220113150212_add_everythingto_orders.rb
+++ db/migrate/20220113150212_add_everythingto_orders.rb
@@ -4,9 +4,9 @@ class AddEverythingtoOrders < ActiveRecord::Migration[6.1]
     add_column :orders, :address, :string
     add_column :orders, :city, :string
     add_column :orders, :zip_code, :integer
-    add_column :orders, :card_number, :integer, limit: 16
+    add_column :orders, :card_number, :integer
     add_column :orders, :exp_date, :date
-    add_column :orders, :cvv, :integer, limit: 3
+    add_column :orders, :cvv, :integer
     add_column :orders, :phone, :string
     add_column :orders, :comment, :text
     add_reference :orders, :user
