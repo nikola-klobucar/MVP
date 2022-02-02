159c1b4860d2a7683532af9634b17847809525c6
nikola-klobucar <nikola.klobucar1@gmail.com>
2022-02-02 16:21:04 +0100

    Added PaymentsController Test

--- test/controllers/payments_controller_test.rb
+++ test/controllers/payments_controller_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class PaymentsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    sign_in users(:foo)
+  end
+
+  test "can get to the new payment and get the JS form" do
+    post orders_url params: {
+        order: {
+          currency: CURRENCY, address: "Address 2",
+          city: "City 2", zip_code: 10000, phone: "003851111111", 
+          transaction_type: "authorize"
+        }
+      }
+    get new_payment_url
+    assert_response :success
+    assert_select "label", "Credit or debit card"
+  end
 end
